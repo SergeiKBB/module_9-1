8b072a588e6bd50d002397ca9db0bf39afec365c
Serhii_Khomych <Serhii.Khomych@refinitiv.com>
2021-02-22 20:41:05 +0200

    first commit

---
+++ calculator.js
@@ -0,0 +1,9 @@
+function sum(a, b) {
+    return a + b;
+}
+
+module.exports = {
+    sum,
+    subtract: (a, b) => a - b,
+    PI: Math.PI
+};
---
+++ index.js
@@ -0,0 +1,5 @@
+const _ = require('lodash');
+const calculator = require("./calculator");
+
+
+console.log(calculator.sum(2, 5));
